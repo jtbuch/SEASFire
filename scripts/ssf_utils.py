1b23d510d685f041161d5cc9a4da6b428be10811
jtbuch <jb4625@columbia.edu>
2023-09-01 16:43:55 -0400

    first 2023 forecasts

--- scripts/ssf_utils.py
+++ scripts/ssf_utils.py
@@ -362,7 +362,7 @@ def fire_prob_pred_func(freq_id= None, seed= None, X_tot_df= None, X_test_df= No
         coords=dict(
             X=(["X"], np.linspace(-2349250, -501250, 155)),
             Y=(["Y"], np.linspace(3166500, 682500, 208)),
-            time= (["month"], np.linspace(0, len(pred_mon_arr) - 1, len(pred_mon_arr), dtype= np.int64)),),)
+            time= (["month"], pred_mon_arr),),) #np.linspace(0, len(pred_mon_arr) - 1, len(pred_mon_arr), dtype= np.int64)
     
     if sav_flag:
         if firemon_pred_flag == 'observations':
@@ -381,8 +381,8 @@ def mon_fire_prob_pred(freq_id= '08_07_23', seed= 654, plot_yr= 2019, fmon= 5, f
 
     freq_id, seed: id and seed of trained ML freq model
     plot_yr: year for which predictions are made
-    fmon: month for which predictions are made
-    fire_df: dataframe containing observed number of fires;  January --> 0, February --> 1, etc.
+    fmon: month for which predictions are made;  January --> 0, February --> 1, ..., June --> 5 etc.
+    fire_df: dataframe containing observed number of fires
     firemon_pred_flag: flag to indicate whether predictions are made for observations, dynamical forecasts or statistical forecasts
     ens_no: ensemble member number for dynamical or statistical forecasts
     pred_fire_df: dataframe containing predicted fire probability
@@ -401,11 +401,54 @@ def mon_fire_prob_pred(freq_id= '08_07_23', seed= 654, plot_yr= 2019, fmon= 5, f
     baseline_arr= np.arange(209, 426, 12)
     n_fires_baseline= len(fire_df[fire_df['fire_month'].isin(baseline_arr + (fmon - 5))])/20
 
-    pred_prob_baseline= xarray.open_dataarray('../sav_files/ssf_pred_files/pred_prob_xarr_rs_%s'%freq_id + '_%d_'%seed + 'obs_baseline.nc')
-    pred_prob_xarr_baseline= pred_prob_baseline[(fmon - 5)::3, :, :].mean(dim= 'month')
-        
+    pred_prob_baseline= xarray.open_dataarray('../sav_files/ssf_pred_files/pred_prob_xarr_%s'%freq_id + '_%d_'%seed + 'obs_baseline.nc')
+    pred_prob_xarr_baseline= pred_prob_baseline[pred_prob_baseline.time.isin(baseline_mon_arr_func(start_yr= 2001, end_yr= 2019, mindx= (fmon + 1)).values), :, :].mean(dim= 'month')
+    #fmon + 1 because months start from 1 in baseline formulation    
     return 10**(np.log10(pred_prob_xarr[(fmon - 5), :, :]) - np.log10(pred_prob_xarr_baseline) - np.log10(n_fires_baseline/n_fires_yr))
 
+def ens_mon_fire_prob_pred(freq_id= '08_07_23', seed= 654, plot_yr= 2019, smon= 4, fmon= 5, fire_df= None, firemon_pred_flag= 'dynamical_forecasts', statistic= 'mean'):
+    """
+    Function to rescale predicted fire probability with climatological baseline fire probability and observed number of fires
+
+    freq_id, seed: id and seed of trained ML freq model
+    plot_yr: year for which predictions are made
+    smon: start month of baseline array;  January --> 0, February --> 1, ..., May --> 4 etc.
+    fmon: month for which predictions are made;  January --> 0, February --> 1, ..., June --> 5 etc.
+    fire_df: dataframe containing observed number of fires
+    firemon_pred_flag: flag to indicate whether predictions are made for observations, dynamical forecasts or statistical forecasts
+    """
+    
+    if firemon_pred_flag == 'dynamical_forecasts':
+        ens_pred_prob_xarr= xarray.concat([xarray.open_dataarray('../sav_files/ssf_pred_files/dynamical_forecasts/pred_prob_xarr_%s'%freq_id + '_%d_'%seed + \
+                                                                                            'df_%d'%ens_no + '_%s.nc'%plot_yr) for ens_no in range(51)], dim= 'ens_no')
+        mdn_tot_df= pd.DataFrame([])
+        for ens_no in range(51):
+            mdn_tmp_df= pd.read_hdf('../sav_files/fire_freq_pred_dfs/dynamical_forecasts/mdn_ssf_%s'%freq_id + '_%d'%seed +  '_fire_freq_%d'%ens_no + '_%d.h5'%plot_yr)
+            mdn_tot_df= pd.concat([mdn_tot_df, mdn_tmp_df], axis= 0)
+        pred_fire_df= mdn_tot_df.groupby(mdn_tot_df.index).mean().round().astype(int)
+        if statistic == 'mean':
+            pred_prob_xarr= ens_pred_prob_xarr.mean(dim= 'ens_no')
+        elif statistic == 'std':
+            pred_prob_xarr= ens_pred_prob_xarr.std(dim= 'ens_no')[(fmon - smon):(fmon - smon)+2].mean(dim= 'month')
+        n_fires_yr= pred_fire_df[pred_fire_df.month == (plot_yr - 1984)*12 + fmon]['pred_mean_freq'].sum()
+    else:
+        ens_pred_prob_xarr= xarray.concat([xarray.open_dataarray('../sav_files/ssf_pred_files/statistical_forecasts/pred_prob_xarr_%s'%freq_id + '_%d_'%seed + \
+                                                                                            'df_%d'%ens_no + '_%s.nc'%plot_yr) for ens_no in range(51)], dim= 'ens_no')
+        pred_prob_xarr= xarray.open_dataarray('../sav_files/ssf_pred_files/statistical_forecasts/pred_prob_xarr_%s'%freq_id + '_%d_'%seed + 'sf_%d'%ens_no + '_%s.nc'%plot_yr)
+        n_fires_yr= pred_fire_df[pred_fire_df.month == (plot_yr - 1984)*12 + fmon]['pred_mean_freq'].sum()
+
+    baseline_arr= baseline_mon_arr_func(start_yr= 2001, end_yr= 2019, mindx= smon).values
+    n_fires_baseline= len(fire_df[fire_df['fire_month'].isin(baseline_arr + (fmon - smon))])/20
+
+    pred_prob_baseline= xarray.open_dataarray('../sav_files/ssf_pred_files/pred_prob_xarr_%s'%freq_id + '_%d_'%seed + 'obs_baseline.nc')
+    pred_prob_xarr_baseline= pred_prob_baseline[pred_prob_baseline.time.isin(baseline_mon_arr_func(start_yr= 2001, end_yr= 2019, mindx= fmon).values), :, :].mean(dim= 'month')
+    #fmon + 1 because months start from 1 in baseline formulation
+
+    if statistic == 'mean':   
+        return 10**(np.log10(pred_prob_xarr[(fmon - smon), :, :]) - np.log10(pred_prob_xarr_baseline) - np.log10(n_fires_baseline/n_fires_yr))
+    elif statistic == 'std':
+        return 10**(np.log10(pred_prob_xarr) - np.log10(pred_prob_xarr_baseline) - np.log10(n_fires_baseline/n_fires_yr))
+    
 def grid_ssf_freq_predict(X_test_dat, freq_test_df= None, n_regs= 18, ml_model= None, func_flag= 'zipd', pred_mon_flag= True, pred_mons= None, seas_flag= None,\
                                                                                     base_yr= 1984, fcast_yr= None, loc_flag= False, rseed= 99):
     
